6f776e84180c15789ff235e630598e1f347a3928
William Eduardo Meza <williameduardomeza@gmail.com>
2018-09-18 12:24:14 -0500

    Add basic Validation

--- app/Http/Controllers/Send2EmailController.php
+++ app/Http/Controllers/Send2EmailController.php
@@ -3,17 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Validator;
 
 class Send2EmailController extends Controller
 {
     /**
      * Show the email for the given data.
      *
-     * @param  var  $email
+     * @param  string  $email
      * @return Response
      */
-    public function __invoke($email)
+    public function __invoke(string $email, Request $request)
     {
-        return $email;
+        
+        $data = array_merge($request->all(), ['email' => $email]);
+
+        $validator = Validator::make($data, [
+            'email' => 'required|email',
+            '_replyto' => 'nullable|email',
+            '_cc'   =>  'nullable|email',
+            '_next' => 'nullable|url',
+            '_email'    => 'nullable|email|max:191',
+            '_name'     => 'nullable|max:191',
+            '_subject'  => 'nullable|max:191',
+            '_message'  => 'nullable|max:191',
+            '_phone'    => 'nullable|max:191'
+        ]);
+
+        if ($validator->fails()) {
+            return 'error';
+        }
+
+        return $data;
     }
 }
