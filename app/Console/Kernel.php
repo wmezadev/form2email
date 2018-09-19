cc7ce2f8b0241fe6bc40662d291c9af68594dd84
William Eduardo Meza <williameduardomeza@gmail.com>
2018-09-19 10:24:33 -0500

    Revert "fix schedule commands"
    This reverts commit 8b182c49b9410c9db4e5981aedbb2e0145889bbf.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,7 +2,6 @@
 
 namespace App\Console;
 
-use App\Console\Commands\EnsureQueueWorkerIsRunning;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -20,12 +19,23 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
-     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('queue:checkup')->everyFiveMinutes();
+    }
+
+    /**
+     * Register the commands for the application.
+     *
+     * @return void
+     */
+    protected function commands()
+    {
+        $this->load(__DIR__.'/Commands');
+
+        require base_path('routes/console.php');
     }
 }
