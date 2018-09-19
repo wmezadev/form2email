aa1cc9e1557b989aed0f8ba23c2c3531e3143ca0
William Eduardo Meza <williameduardomeza@gmail.com>
2018-09-19 10:02:43 -0500

    Use https://gist.github.com/crishoj/79c6a708dbd90ada4b5f3911c473baf2

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        EnsureQueueWorkerIsRunning::class
     ];
 
     /**
@@ -24,9 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('queue:work --tries=3')
-                ->cron('* * * * * *')
-                ->withoutOverlapping();
+        $schedule->command('queue:checkup')->everyFiveMinutes();
     }
 
     /**
